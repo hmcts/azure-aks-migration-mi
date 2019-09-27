12b3ce417dac871fcc5a3853c0350f3a78a22e46
Tim Jacomb <tim.jacomb@hmcts.net>
2019-09-27 12:45:12 +0100

    Use https for checkout and prod storage account

--- update-all.sh
+++ update-all.sh
@@ -6,7 +6,7 @@ az webapp list --subscription DCD-CNP-PROD --query '[].name' -o tsv | sort | sed
 
 ##### AKS #####
 rm -rf cnp-flux-config/
-git clone --depth 1 git@github.com:hmcts/cnp-flux-config
+git clone --depth 1 https://github.com/hmcts/cnp-flux-config.git
 
 rg --no-line-number --no-heading  --no-filename "releaseName: (.*)" -r '$1' cnp-flux-config/k8s/prod/common | awk '{$1=$1};1' > aks-apps.txt
 rm -rf cnp-flux-config/
@@ -16,4 +16,4 @@ rm -f ase-merged.csv
 echo '"Product","App","Where","Tier"' > ase-merged.csv
 ./index.js >> ase-merged.csv
 
-az storage blob upload --account-name mgmtstatestoresandbox --container-name aks-migration-mi --name ase-merged.csv  --file ase-merged.csv --subscription DCD-CFT-Sandbox
+az storage blob upload --account-name mgmtstatestoreprod --container-name aks-migration-mi --name ase-merged.csv  --file ase-merged.csv --subscription DCD-CNP-Prod
